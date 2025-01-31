ea0afa8df199f8f22529a9c6c93169459f44161c
unknown <santimallalakshmidhar@gmail.com>
2025-01-31 09:54:47 +0530

    ffs

---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+import { Route, Routes } from 'react-router-dom'
+import LoginPage from './routes/Routes'
+function App() {
+  
+
+  return (
+    <>
+    zx<Routes>
+      <Route path='/' element={<LoginPage/>} />
+
+      
+    </Routes>
+     
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/routes/Routes.jsx
@@ -0,0 +1,4 @@
+import LoginPage from "../page/loginPage";
+
+
+export {LoginPage}
